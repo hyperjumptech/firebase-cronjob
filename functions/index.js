c7dfb0d518a28ceff9a1a7f80927f35a5763654e
Ariya Hidayat <ariya@hyperjump.tech>
2020-04-07 22:44:03 -0700

    Cloud function: showJobCounter

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,21 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
 exports.ping = functions.https.onRequest((request, response) => {
     response.send(`OK ${Date.now()}`);
+});
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+exports.showJobCounter = functions.https.onRequest(async (request, response) => {
+    try {
+        var doc = await db.collection('jobConfig').doc('counter').get();
+        var counter = doc.data().value;
+        console.log(`Counter is ${counter}`);
+        response.send(`Counter is ${counter}`);
+    } catch (err) {
+        console.error(`Failed to obtain the counter: ${err.toString()}`);
+        response.send(`EXCEPTION: ${err.toString()}`);
+    }
 });
--- tests/integration_tests.js
+++ tests/integration_tests.js
@@ -10,3 +10,11 @@ describe('firebase-cronjobs', function () {
         assert(/[0-9]+/.test(timestamp));
     });
 });
+
+describe('firebase-cronjobs', function () {
+    it('should have a working showJobCounter function', async function () {
+        const res = await axios.get('http://localhost:5000/showJobCounter');
+        // fresh run: jobConfig/counter does not exist yet
+        assert.equal(res.data, "EXCEPTION: TypeError: Cannot read property 'value' of undefined");
+    });
+});
